cd2cfaa6e83bb0b2ac8e28595957fd96dcb84f35
Bilong Emmanuel <emmanueldigital9@gmail.com>
2025-02-20 10:15:35 +0100

    Initialisation du projet

---
+++ src/main/java/com/enigma/automated_resume_screening/AutomatedResumeScreeningApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.automated_resume_screening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedResumeScreeningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedResumeScreeningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enigma/automated_resume_screening/AutomatedResumeScreeningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.automated_resume_screening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedResumeScreeningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
